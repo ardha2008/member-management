43c6f5539b00ae2b1706c3dda4441300dcd76d28
Ardha Herdianto <ardha2008@gmail.com>
2014-07-15 01:30:54 +0700

    Penambahan tabel
    Penambahan tabel sql dan referal

--- member-management.sql
+++ member-management.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.32)
-# Date: 2014-06-28 17:19:13
+# Date: 2014-07-15 01:30:35
 # Generator: MySQL-Front 5.3  (Build 4.100)
 
 /*!40101 SET NAMES utf8 */;
@@ -11,13 +11,14 @@
 DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
   `nama` varchar(255) DEFAULT NULL,
   `deskripsi` text,
   `level` varchar(255) DEFAULT NULL COMMENT '4= Superadmin, 3=VIP , 2=Member lv 1 ,1= Register',
   `path` varchar(255) DEFAULT NULL,
   `tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
 # Data for table "files"
@@ -30,19 +31,36 @@ CREATE TABLE `files` (
 
 DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) NOT NULL DEFAULT '',
+  `username` varchar(255) NOT NULL DEFAULT '',
   `email` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
-  `nama` varchar(255) DEFAULT '-',
+  `nama` varchar(255) NOT NULL DEFAULT '-',
   `alamat` varchar(255) DEFAULT NULL,
   `banned` enum('Y','N') DEFAULT 'N',
   `level` varchar(1) DEFAULT '1' COMMENT '4= Superadmin, 3=VIP , 2=Member lv 1 ,1= Register',
   `tgl_register` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
 # Data for table "member"
 #
 
-INSERT INTO `member` VALUES (9,'ardha2008@yahoo.com','lLy9r6g=620adc1433d2e8794f9b87569a43228bbEx5OXI2Zz0=','-',NULL,'N','4','2014-06-28 00:34:24'),(10,'admin@localhost.com','lLy9r6g=620adc1433d2e8794f9b87569a43228bbEx5OXI2Zz0=','Admin','','N','4','2014-06-28 00:36:18'),(11,'user@user.com','ZHyMew==62a9f2195c3f3865f88f5fb260d79a62Wkh5TWV3PT0=','user','user','N','1','2014-06-28 13:20:56');
+INSERT INTO `member` VALUES ('53c4161704af2','ardha2008','ardha@gmail.com','YpjHnJSWn5ybnp2cz56modajpqqqp6eo296rZJeZlpk=','-',NULL,'N','1','2014-07-15 00:40:40');
+
+#
+# Structure for table "referal"
+#
+
+DROP TABLE IF EXISTS `referal`;
+CREATE TABLE `referal` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "referal"
+#
+
