4ae5d484b5331f56d037f960140a240d570bb772
Ardha Herdianto <ardha2008@gmail.com>
2014-07-15 17:06:41 +0700

    Disable index of

--- app/views/content/profile/index.php
+++ app/views/content/profile/index.php
@@ -11,7 +11,7 @@
                 <div class="media">
     				 <a href="<?= $this->location('myfiles/detail/'.$row->id) ?>" class="pull-left"><img src="<?= $this->uri->getBaseUri() ?>assets/img/preview.png" class="media-object" alt='' /></a>
     				<div class="media-body">
-    					 <?= $row->deskripsi ?>
+    					 <?= $this->libraries->limiter->word_limiter($row->deskripsi,70) ?>
     				</div>
     			</div>
             </div>
